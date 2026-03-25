535c2d31bcd55fcc3da29bbe56b429a65a865963
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2026-03-25 11:59:55 +0800

    Update tab.py

--- pydoll/browser/tab.py
+++ pydoll/browser/tab.py
@@ -2142,10 +2142,11 @@ class Tab(FindElementsMixin):
             # logger.info(f"[BYPASS] checkbox._mouse = {checkbox._mouse}")
             # 最后再点击
             bounds = await checkbox.get_bounds_using_js()
-            cx = bounds['x'] + bounds['width'] / 2
-            cy = bounds['y'] + bounds['height'] / 2
+            await self.mouse.click(
+                 bounds['x'] + bounds['width'] / 2,
+                 bounds['y'] + bounds['height'] / 2,
 
-            await self.mouse.click(cx, cy)
+            )
             logger.info("[BYPASS] ✅ checkbox clicked")
 
             # 等待验证生效
